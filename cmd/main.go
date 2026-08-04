60b7a311f4e2013c41fcbaa6d6b83af2890aeaaa
haarshmap <harshmap0@gmail.com>
2026-08-04 21:16:39 +0530

    feat: added the users table and routes

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
 	"os"
 
@@ -13,8 +15,15 @@ import (
 	"github.com/uptrace/bun/driver/sqliteshim"
 )
 
+type Users struct {
+	ID       int64  `bun:"id,pk,autoincrement"`
+	Username string `bun:"username,unique,notnull"`
+	Password string `bun:"password,notnull"`
+}
+
 func main() {
 	var err error
+	ctx := context.Background()
 	r := chi.NewRouter()
 
 	sqlite, err := sql.Open(sqliteshim.ShimName, "data.db")
@@ -24,11 +33,45 @@ func main() {
 
 	db := bun.NewDB(sqlite, sqlitedialect.New())
 
+	_, err = db.NewCreateTable().Model((*Users)(nil)).IfNotExists().Exec(ctx)
+
 	if err := db.Ping(); err != nil {
 		log.Fatalf("Database ping failed: %v", err)
 	} else {
 		fmt.Println("SQLite database initialized successfully with Bun!")
 	}
 
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World"))
+	})
+
+	//routes
+	r.Post("/register", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("currently at register"))
+		name := r.FormValue("username")
+		password := r.FormValue("password")
+		user := &Users{Username: name, Password: password}
+		_, err = db.NewInsert().Model(user).Exec(ctx)
+		if err != nil {
+			slog.Log(r.Context(), 0, "failed to initialise row", err)
+		}
+	})
+	r.Post("/login", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("currently at login"))
+		name := r.FormValue("username")
+		password := r.FormValue("password")
+		user := new(Users)
+		slog.Log(r.Context(), 0, "just checking out user", user)
+		err = db.NewSelect().Model(user).Where("Username = ?", name).Scan(ctx)
+
+		if password == user.Password {
+			fmt.Println("User is logged in")
+		} else {
+			fmt.Println("User aint logged in twin")
+		}
+
+	})
+
 	http.ListenAndServe(":"+os.Getenv("PORT"), r)
+
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,14 @@ module github.com/haarshmap/chatrbox
 go 1.26.5
 
 require (
+	github.com/go-chi/chi/v5 v5.3.1
+	github.com/uptrace/bun v1.2.18
+	github.com/uptrace/bun/dialect/sqlitedialect v1.2.18
+	github.com/uptrace/bun/driver/sqliteshim v1.2.18
+)
+
+require (
 	github.com/dustin/go-humanize v1.0.1 // indirect
-	github.com/go-chi/chi/v5 v5.3.1 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -13,9 +19,6 @@ require (
 	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
-	github.com/uptrace/bun v1.2.18 // indirect
-	github.com/uptrace/bun/dialect/sqlitedialect v1.2.18 // indirect
-	github.com/uptrace/bun/driver/sqliteshim v1.2.18 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
 	golang.org/x/exp v0.0.0-20260218203240-3dfff04db8fa // indirect
